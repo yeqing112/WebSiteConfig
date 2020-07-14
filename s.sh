4f8e96fb939c31969e8e51f283574d2b0bc0ae80
yeqing112 <41815786+yeqing112@users.noreply.github.com>
2020-07-14 23:36:13 +0800

    Update s.sh

--- s.sh
+++ s.sh
@@ -8,8 +8,8 @@ mkdir data
 # 创建容器网络
 docker network create myproxy
 # 启动数据库
-docker run -d --name mysql57 -e MYSQL_ROOT_PASSWORD=3165ilcy -p 3306:3306 -v $PWD/data:/var/lib/mysql --network=myproxy --restart=always registry.cn-shanghai.aliyuncs.com/yeqing112/mysql:5.7
+docker run -dit --name mysql57 -e MYSQL_ROOT_PASSWORD=3165ilcy -p 3306:3306 -v $PWD/data:/var/lib/mysql --network=myproxy --restart=always registry.cn-shanghai.aliyuncs.com/yeqing112/mysql:5.7
 # 启动php7.2环境
-docker run -d --name php_fpm -v $PWD/www:/data/www --network=myproxy --restart=always registry.cn-shanghai.aliyuncs.com/yeqing112/php:7.3-fpm-alpine
+docker run -dit --name php_fpm -v $PWD/www:/data/www --network=myproxy --restart=always registry.cn-shanghai.aliyuncs.com/yeqing112/php:7.3-fpm-alpine
 # 启动nginx
-docker run -d --name nginx -p 80:80 -p 443:443 -v $PWD/www:/data/www -v $PWD/nginx/conf.d:/etc/nginx/conf.d -v $PWD/nginx/fastcgi.conf:/etc/nginx/fastcgi.conf -v $PWD/nginx/rewrite.conf:/etc/nginx/rewrite.conf -v $PWD/ssl:/etc/nginx/ssl --network=myproxy --restart=always registry.cn-shanghai.aliyuncs.com/yeqing112/nginx:1.19.1
+docker run -dit --name nginx -p 80:80 -p 443:443 -v $PWD/www:/data/www -v $PWD/nginx/conf.d:/etc/nginx/conf.d -v $PWD/nginx/fastcgi.conf:/etc/nginx/fastcgi.conf -v $PWD/nginx/rewrite.conf:/etc/nginx/rewrite.conf -v $PWD/ssl:/etc/nginx/ssl --network=myproxy --restart=always registry.cn-shanghai.aliyuncs.com/yeqing112/nginx:1.19.1
