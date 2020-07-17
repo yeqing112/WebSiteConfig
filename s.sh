4eb8a872d40162d8ddafa0f88f494e66a4fd6f1a
yeqing112 <41815786+yeqing112@users.noreply.github.com>
2020-07-17 18:30:20 +0800

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-INSTALLSH_VERSION=1.0.9
+INSTALLSH_VERSION=1.0.10
 cd ~
 wget https://github.com/yeqing112/webSiteConfig/archive/${INSTALLSH_VERSION}.tar.gz \
 && tar -zxvf ${INSTALLSH_VERSION}.tar.gz
